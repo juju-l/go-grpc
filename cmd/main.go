b186eece8c33eb7d3df3b72c0a79771f168e978c
root <lj@vipex.cc>
2021-03-11 18:06:30 +0800

    [重构项目结构]

--- cmd/main.go
+++ cmd/main.go
@@ -1,35 +1,4 @@
 package main
 
-import (
-	"gitee.com/vipex/go-grpc/configs/etcd"
-	"gitee.com/vipex/go-grpc/internal/handler"
-	"gitee.com/vipex/go-grpc/utils"
-	"github.com/micro/go-micro/v2"
-	"github.com/micro/go-micro/v2/logger"
-	"github.com/micro/go-micro/v2/registry"
-	"github.com/micro/go-micro/v2/registry/etcd"
-)
-
 func main() {
-	microReg := etcd.NewRegistry(
-		registry.Addrs((*utils.GetGlobal())["etcd"].(*configs.EtcdAddrCfg).Addr),
-	)
-
-	// New Service
-	service := micro.NewService(
-		micro.Name("sea2.micro.api.client_manage"),
-		micro.Version("latest"),
-		micro.Registry(microReg),
-	)
-
-	// Initialise service
-	service.Init()
-
-	// Register Handler
-	handler.RegisterUser(service.Server())
-
-	// Run service
-	if err := service.Run(); err != nil {
-		logger.Fatal(err)
-	}
 }
---
+++ internal/domain/dao/user_dao.go
@@ -0,0 +1,7 @@
+package dao
+
+type User struct {
+	Id int `json:"id"`
+	User string `json:"user"`
+	V bool `json:"v"`
+}
