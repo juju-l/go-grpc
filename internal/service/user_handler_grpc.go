2243546585dc9da2e479eb8248e04ec4708235db
root <lj@vipex.cc>
2021-03-12 09:38:59 +0800

    [修复一些问题]

--- internal/service/user_handler_grpc.go
+++ internal/service/user_handler_grpc.go
@@ -1,17 +1,18 @@
 package service
 
 import (
+	"context"
 	pb "gitee.com/vipex/go-grpc/api/vipex.cc/oauth2/v1/v1.proto"
 	"gitee.com/vipex/go-grpc/internal/domain/dao"
 	"gitee.com/vipex/go-grpc/internal/usecase"
 )
 
-func (rpc *UserGrpcHandler) Login(user pb.User) (pb.BaseResult, error) {
+func (rpc *UserGrpcHandler) Login(ctx context.Context, user *pb.User, baseResult *pb.BaseResult) error {
 	var rst = pb.BaseResult{}
 	if r, err := new(usecase.UserUseCase).Login(&dao.UserReq{user.User, user.Pswd}); err != nil {
 		rst.Data = r
 	}
-	return rst, nil
+	baseResult = &rst; return nil
 }
 
 type UserGrpcHandler struct {
