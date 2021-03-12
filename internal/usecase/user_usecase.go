123a4e948d1e44bf3d153f747e39119af30a2553
root <lj@vipex.cc>
2021-03-12 18:29:26 +0800

    [修复一个错误]

--- internal/usecase/user_usecase.go
+++ internal/usecase/user_usecase.go
@@ -11,7 +11,7 @@ func (use *UserUseCase) Login(req *dao.UserReq) (bool, error) {
 	 * 该 userCase 层主要组装所有的业务逻辑
 	 * 该单一业务直接返回，不表示该分层仅执行该内容，复杂业务时，在该服务层中，需继续其他实现
 	***/
-	return new(repoistory.UserRepository).Login(req)
+	return new(repository.UserRepository).Login(req)
 }
 
 type UserUseCase struct {
