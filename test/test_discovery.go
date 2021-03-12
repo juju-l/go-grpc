df98bed1b1130ebf3f8b808a71674e6b879473c5
root <lj@vipex.cc>
2021-03-12 13:07:38 +0800

    【添加 go-micro 客户端测试】

---
+++ test/test_discovery.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	v1_proto "gitee.com/vipex/go-grpc/api/vipex.cc/oauth2/v1/v1.proto"
+	pri "gitee.com/vipex/go-grpc/internal/domain/interface"
+	"github.com/micro/go-micro/v2"
+	"github.com/micro/go-micro/v2/registry"
+	"github.com/micro/go-micro/v2/registry/etcd"
+	"fmt"
+)
+
+func main() {
+	etcdRegistry := etcd.NewRegistry(
+		registry.Addrs("127.0.0.1:2379"),
+	)
+
+	service := micro.NewService(
+		micro.Name("cc.vipex.client.o2"),
+		micro.Registry(etcdRegistry),
+	)
+
+	service.Init()
+
+	userClient := pri.NewUserGrpcService("cc.vipex.service.o2", service.Client())
+
+	rsp, err := userClient.Login(context.TODO(), &v1_proto.User{
+		User: "admin", Pswd: "654321",
+	})
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(rsp)
+}
