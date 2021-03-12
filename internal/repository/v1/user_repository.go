242981c7fe1e9618490edb2e464690fd128d8963
root <lj@vipex.cc>
2021-03-12 18:09:48 +0800

    [修复错误]

--- internal/repository/v1/user_repository.go
+++ internal/repository/v1/user_repository.go
@@ -9,8 +9,8 @@ import (
 
 func (s *UserRepository) Login(req *v1_dao.UserReq) (bool, error) {
 	var db = (*utils.GetGlobal())["configs"].(*v1_dao.AppConfigs).GetCrudRepo()
-	var userInfo v1_model.User;args := "\"user\" = @User AND \"pswd\" = @Pswd" // where 入参(pg 有转义)
-		err := db.Where(args, login_Req).First(&userInfo).Error // FirstOrInit 第一值
+	var userInfo v1_model.User; args := "\"user\" = @User AND \"pswd\" = @Pswd" // where 入参(pg 有转义)
+		err := db.Where(args, req).First(&userInfo).Error // FirstOrInit
 	if err == nil && userInfo.V == true { return true, nil } // 用户登录验证通过 - 产生 token
 	return false, nil
 }
