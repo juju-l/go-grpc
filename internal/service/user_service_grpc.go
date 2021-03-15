6ad67530629ba7f5fc2d574f398a1c39ef43b57c
root <lj@vipex.cc>
2021-03-15 20:20:19 +0800

    修复一些问题

--- internal/service/user_service_grpc.go
+++ internal/service/user_service_grpc.go
@@ -1,20 +1,17 @@
 package service
 
 import (
+	"context"
 	pb "gitee.com/vipex/go-grpc/api/vipex.cc/oauth2/proto"
 	"gitee.com/vipex/go-grpc/internal/domain/dao"
-	// pri "gitee.com/vipex/go-grpc/internal/domain/interface"
 	"gitee.com/vipex/go-grpc/internal/usecase"
 )
 
-func (rpc *UserServiceGrpc) Login(user pb.User) (pb.BaseResult, error) {
-	var rst = pb.BaseResult{}
-	if r, err := new(usecase.UserUseCase).Login(&dao.UserReq{user.User, user.Pswd}); err != nil {
-		rst.Data = r
-	}
+func (rpc *UserServiceGrpc) Login(ctx context.Context, user *pb.User) (*pb.BaseResult, error) {
+	var rst = &pb.BaseResult{}
+	if r, err := new(usecase.UserUseCase).Login(&dao.UserReq{user.User, user.Pswd}); err == nil { rst.Data = r }
 	return rst, nil
 }
 
 type UserServiceGrpc struct {
-	// pri.UnimplementedUserGrpcServer
 }
