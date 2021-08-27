efb5b22e01d744c52191af94cc0e02c3c2614ee0
root <lj@vipex.cc>
2021-08-27 16:01:47 +0800

    调整格式

--- cmd/grpc.go
+++ cmd/grpc.go
@@ -1,26 +1,18 @@
 package main
 
 import (
-	"flag"
+	"net"
+
 	v1_interface "codeup.aliyun.com/vipex/go-grpc/internal/domain/v1/v1.interface"
 	v1_service "codeup.aliyun.com/vipex/go-grpc/internal/service/v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
-	"net"
 )
 
 func main() {
-	listenAddr := ""
-	flag.StringVar(&listenAddr, "addr", ":8080", "app start listen addr...") // 获取参数值
-	flag.Parse()
-
-	lis, _ := net.Listen("tcp", listenAddr)
-
+	listen, _ := net.Listen("tcp", ":8080")
 	s := grpc.NewServer()
-	v1_interface.RegisterUserGrpcServer(s, &v1_service.UserServiceGrpc{}) // 注册服务
-
-
-	reflection.Register(s) // 添加反射的注册，给 grpcui 提供入口
-
-	s.Serve(lis) // 
+	v1_interface.RegisterUserGrpcServer(s, &v1_service.UserServiceGrpc{})
+	reflection.Register(s) // 反射
+	s.Serve(listen) 
 }
