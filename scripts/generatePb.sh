85a9861571782f9c6d45f08424853fb74ab0b1c8
root <lj@vipex.cc>
2021-08-27 22:29:45 +0800

    base pb code gen

--- scripts/generatePb.sh
+++ scripts/generatePb.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+
+protoc -I . --go_out . --go_opt=paths=source_relative --go-grpc_out . --go-grpc_opt=paths=source_relative --go-grpc_opt=require_unimplemented_servers=false api/vipex.cc/*/v1/v1.proto/*.proto
