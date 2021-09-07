2559a42892258a34bac3a7575d590bf0115e928b
root <lj@vipex.cc>
2021-09-08 06:47:13 +0800

    调整

--- scripts/generatePb.sh
+++ scripts/generatePb.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-protoc -I . --go_out . --go_opt=paths=source_relative --go-grpc_out . --go-grpc_opt=paths=source_relative --go-grpc_opt=require_unimplemented_servers=false api/vipex.cc/*/v1/v1.proto/*.proto
+find ./api/ -name *.go -exec rm -f {} \;
+
+protoc -I . \
+--go_out . --go_opt=paths=source_relative \
+--go-grpc_out . --go-grpc_opt=paths=source_relative \
+--go-grpc_opt=require_unimplemented_servers=false \
+api/vipex.cc/*/v1/v1.proto/*.proto
+
+
+tree api/vipex.cc
+#...
