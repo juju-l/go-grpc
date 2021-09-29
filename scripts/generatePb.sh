5b9d9e1181b85a21cc4fd152c3ea0802327c3bb2
root <lj@vipex.cc>
2021-09-29 09:06:54 +0800

    完善pb生成脚本

--- scripts/generatePb.sh
+++ scripts/generatePb.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-find ./api/ -name *.go -exec rm -f {} \;
+find ./api/ -name *.pb.go \
+-exec rm -f {} \;
 
-protoc -I . \
+protoc -I third_party -I . \
 --go_out . --go_opt=paths=source_relative \
 --go-grpc_out . --go-grpc_opt=paths=source_relative \
 --go-grpc_opt=require_unimplemented_servers=false \
